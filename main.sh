f75b999f3ab1d9482024805b1e774ac5bb1d4b08
Dragos Dumitrache <dragosd2000+github@gmail.com>
2022-08-23 10:37:39 +0100

    Adding support for nested doc dirs (#8)
    This has been successfully tested with our knowledge base and shows up on backstage with the corresponding subcomponents.
    
    Should be backwards compatible with the existing, top dir, workflow, as no defaults have been changed, just made explicit

--- main.sh
+++ main.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
 set -eo pipefail
-while getopts a:b:c:d:e:f:g:h flag
+while getopts a:b:c:d:e:f:g:h:i flag
 do
     case "${flag}" in
         a) publisher=${OPTARG};;
         b) credentials=${OPTARG};;
         c) bucket=${OPTARG};;
         d) entity=${OPTARG};;
-        e) azureAccountName=${OPTARG};;
-        f) azureAccountKey=${OPTARG};;
-        g) awsRoleArn=${OPTARG};;
-        h) awsEndpoint=${OPTARG};;
+        e) sourceDir=${OPTARG};;
+        f) azureAccountName=${OPTARG};;
+        g) azureAccountKey=${OPTARG};;
+        h) awsRoleArn=${OPTARG};;
+        i) awsEndpoint=${OPTARG};;
     esac
 done
 
 pushd $GITHUB_WORKSPACE
 
-echo "Generating the docs"
-techdocs-cli generate --no-docker --verbose
+if [[ ! -d $sourceDir ]]
+then
+  echo "$sourceDir does not exist"
+  exit 0
+fi
+echo "Generating the docs from $sourceDir to $sourceDir/site/"
+
+techdocs-cli generate --no-docker --verbose --source-dir "$sourceDir" --output-dir "$sourceDir/site"
 
 if [[ $publisher == "googleGcs" ]]; then
     echo $credentials | base64 -d > /tmp/credentials
@@ -26,7 +33,8 @@ if [[ $publisher == "googleGcs" ]]; then
     GOOGLE_APPLICATION_CREDENTIALS=/tmp/credentials techdocs-cli publish \
         --publisher-type $publisher \
         --storage-name $bucket \
-        --entity $entity
+        --entity $entity \
+        --directory "$sourceDir/site"
 
     exit 0
 fi
@@ -43,7 +51,8 @@ if [[ $publisher == "awsS3" ]]; then
         --storage-name $bucket \
         --entity $entity \ 
         --awsRoleArn $awsRoleArn \
-        --awsEndpoint $awsEndpoint
+        --awsEndpoint $awsEndpoint \
+        --directory "$sourceDir/site"
 fi
 
 if [[ $publisher == "azureBlobStorage" ]]; then
@@ -55,7 +64,8 @@ if [[ $publisher == "azureBlobStorage" ]]; then
         --storage-name $bucket \
         --entity $entity \ 
         --azureAccountName $azureAccountName \
-        --azureAccountKey $azureAccountKey
+        --azureAccountKey $azureAccountKey \
+        --directory "$sourceDir/site"
 
     exit 0
 fi
